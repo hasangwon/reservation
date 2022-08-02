03dddd08941f77e613ae2b8a242cca5a2021e26e
Sangwon Ha <305243@naver.com>
2022-08-03 01:10:21 +0900

    Add : 당일 예약 ticket ui, Aside 컴포넌트 rem으로 수정

--- components/Aside.tsx
+++ components/Aside.tsx
@@ -1,48 +1,44 @@
-/* eslint-disable react/no-unknown-property */
-import Dog from "../public/images/dog.png";
-import Image from "next/image";
+import ReservationTicket from "./ReservationTicket";
+export interface DailyType {
+  id: number;
+  pet_name: string;
+  animal: string;
+  purpose: string;
+  visit_date: string;
+  visit_time: string;
+  owner_name: string;
+  owner_phone: string;
+}
 export default function Aside() {
   return (
-    <div className="w-[402px] h-[962px] m-[60px] p-3 rounded-2xl shadow bg-white text-[#333] font-light ">
+    <div className="w-[25rem] h-[50rem] m-[3.5rem] p-[1.625rem] bg-white text-neutral-dark rounded-2xl shadow">
       {/* 선택 */}
-      <h1 className="mt-1 text-[20px]">당일 예약 리스트</h1>
+      <div className="text-[1.25rem] ">
+        <span className="pb-4 border-b-[0.1875rem] border-neutral-dark">당일 예약 리스트</span>
+      </div>
 
       {/* 데이트 피커 */}
-      <div className="mt-2">datepicker</div>
+      <div className="mt-12 mb-[1.625rem]">datepicker</div>
 
       {/* 예약 티켓 공간 */}
-      <div className="mt-2 p-2">
+      <div className="overflow-scroll h-[40rem]">
         {/* 티켓 1개 */}
-        <div className="relative group border-t">
-          {/* 예약 정보 */}
-          <div className="flex items-center mt-3">
-            <span className="mr-2 bg-purple-300 rounded-md px-2 leading-5 font-normal">미용예약</span>
-            <div className="inline-block w-4">
-              <Image src={Dog} alt="dog" className="w-full h-auto" />
-            </div>
-            <span className="ml-2 text-[14px] font-normal">모랑</span>
-          </div>
-          {/* 보호자 정보 */}
-          <div className="mt-2">
-            <span className="mr-1">01098743299</span>|<span className="text-slate-500 ml-1">하상원 보호자</span>
-          </div>
-          {/* 시간정보 */}
-          <div className="flex mt-3 mb-3">
-            <div className="flex flex-col mr-6">
-              <span className="-mb-1 text-slate-400">Time</span>
-              <span>12:01</span>
-            </div>
-            <div className="flex flex-col">
-              <span className="-mb-1 text-slate-400">Date</span>
-              <span>2022.06.01</span>
-            </div>
-          </div>
-          {/* 예약 변경 버튼 */}
-          <div className="absolute bottom-0 right-0 text-primary border border-primary rounded-2xl p-1 px-2 font-normal">예약 변경</div>
-          {/* 예약 삭제 버튼 */}
-          <div className="absolute top-5 right-4 hidden group-hover:block">삭제</div>
-        </div>
+        {DAILY_DATA_TEST.map((doc: DailyType) => {
+          return <ReservationTicket key={doc.id} doc={doc} />;
+        })}
       </div>
     </div>
   );
 }
+
+const DAILY_DATA_TEST = [
+  { id: 1, pet_name: "크림", animal: "dog", purpose: "접종 예약", visit_date: "2022.08.02", visit_time: "07:00", owner_name: "하상원", owner_phone: "01098743299" },
+
+  { id: 2, pet_name: "우유", animal: "cat", purpose: "미용 예약", visit_date: "2022.08.02", visit_time: "09:00", owner_name: "우혜림", owner_phone: "01042311323" },
+
+  { id: 3, pet_name: "당근", animal: "cat", purpose: "수술 예약", visit_date: "2022.08.02", visit_time: "11:00", owner_name: "이범석", owner_phone: "01014536345" },
+
+  { id: 4, pet_name: "튀김", animal: "dog", purpose: "기타 예약", visit_date: "2022.08.02", visit_time: "14:00", owner_name: "조현우", owner_phone: "01023422343" },
+
+  { id: 5, pet_name: "고로케", animal: "dog", purpose: "진료 예약", visit_date: "2022.08.02", visit_time: "19:00", owner_name: "박상우", owner_phone: "01067223299" },
+];
