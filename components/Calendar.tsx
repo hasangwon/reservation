8d569b66ab5ddbd3aa7ef02710697460655b9598
YejeeCho <choyejee14@gmail.com>
2022-08-03 18:12:05 +0900

    예약항목 캘린더에 표시하기

--- components/Calendar.tsx
+++ components/Calendar.tsx
@@ -3,6 +3,7 @@ import { useEffect, useState } from "react";
 import Image from "next/image";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faAngleLeft, faAngleRight } from "@fortawesome/free-solid-svg-icons";
+import ReservationItem from "./ReservationItem";
 import { deflateRawSync } from "zlib";
 
 const Calendar = () => {
@@ -20,6 +21,11 @@ const Calendar = () => {
 
   const [days, setDays] = useState([sun, mon, tue, wed, thu, fri, sat]);
 
+  const [thisDate, setThisDate] = useState(today);
+  const [selected, setSelected] = useState(false);
+  const [className, setClassName] = useState("");
+  const [purpose, setPurpose] = useState([]);
+
   const getMonth = () => {
     if (days[0].get("year") !== days[6].get("year")) {
       return days[0].format("YYYY.MM") + " ~ " + days[6].format("YYYY.MM");
@@ -29,32 +35,23 @@ const Calendar = () => {
       return days[0].format("YYYY.MM");
     }
   };
-  const [selected, setSelected] = useState("");
-
-  const changeColor = () => {
-    setSelected("w-[3.25rem] h-[1.625rem] bg-primary rounded-[0.313rem]");
-  };
 
   const goToLastWeek = () => {
-    setDays((preDays) => {
-      return preDays.map((day) => day.subtract(7, "day"));
+    setDays((days) => {
+      return days.map((day) => day.subtract(7, "day"));
     });
-    // setMonth((day) => day.subtract(7, "day"));
   };
 
   const goToNextWeek = () => {
-    setDays((nextDays) => {
-      return nextDays.map((day) => day.add(7, "day"));
+    setDays((days) => {
+      return days.map((day) => day.add(7, "day"));
     });
-    // setMonth((day) => day.add(7, "day"));
   };
-
-  // console.log(sun);
 
   return (
     <>
-      <div className="m-10 flex align-middle h-12">
-        <button className=" w-[6.125rem] h-12 shadow-md rounded-3xl">
+      <div className="m-10 flex items-center h-12">
+        <button className=" w-[6.125rem] h-12 shadow-md rounded-3xl mx-3">
           <div className="flex my-3.5 mx-5">
             <Image
               src="/images/calendar_icon.png"
@@ -65,21 +62,21 @@ const Calendar = () => {
             <div className="text-[#7165DB] mx-[0.313rem] font-normal">오늘</div>
           </div>
         </button>
-        <div>
+        <div className="mx-3">
           <FontAwesomeIcon
             icon={faAngleLeft}
-            className="mx-2 text-[#4E515D]"
+            className="mr-[0.7rem] text-[#4E515D]"
             onClick={goToLastWeek}
           />
           <FontAwesomeIcon
             icon={faAngleRight}
-            className="mx-2 text-[#4E515D]"
+            className="ml-[0.7rem] text-[#4E515D]"
             onClick={goToNextWeek}
           />
         </div>
-        <div className="text-3xl text-[#4E515D]">{getMonth()}</div>
+        <div className="text-3xl text-[#4E515D] mx-3">{getMonth()}</div>
       </div>
-      <div className="shadow-md m-10 rounded-2xl w-[67.806rem]">
+      <div className="shadow-md m-10 rounded-2xl w-[67.806rem] bg-white">
         <table className="w-[67.806rem] h-[53.983rem]">
           <tbody>
             <tr>
@@ -90,7 +87,7 @@ const Calendar = () => {
                     key={DAYS[index].id}
                     className="h-20 w-32 text-center border-b border-l border-solid border-gray-200"
                   >
-                    <div className="">
+                    <div>
                       <span className=" mr-2 text-base">{day.get("date")}</span>
                       <span className="text-[#BBBBC6] font-normal text-sm">
                         {DAYS[index].day}
@@ -102,17 +99,35 @@ const Calendar = () => {
             </tr>
             {TIME.map((time) => {
               return (
-                <tr key={time.id}>
+                <tr key={time.id} className="h-[3.625rem]">
                   <th className="border-solid border-gray-200 border-r font-thin text-[0.625rem] text-[#AAADB8] align-top w-[2.713rem]">
                     {time.time}
                   </th>
-                  <th className="boreder-solid border-gray-200 border"></th>
-                  <th className="boreder-solid border-gray-200 border"></th>
-                  <th className="boreder-solid border-gray-200 border"></th>
-                  <th className="boreder-solid border-gray-200 border"></th>
-                  <th className="boreder-solid border-gray-200 border"></th>
-                  <th className="boreder-solid border-gray-200 border"></th>
-                  <th className="boreder-solid border-gray-200 border border-r-0"></th>
+                  {days.map((day, i) => {
+                    return (
+                      <th
+                        className="boreder-solid border-gray-200 border-l border-t"
+                        key={i}
+                      >
+                        <div className="flex w-[8rem] flex-wrap">
+                          {RESERVATION.map((res): any => {
+                            if (
+                              res.date === day.format("YYYY-MM-DD") &&
+                              res.time === time.time
+                            ) {
+                              return (
+                                <ReservationItem
+                                  purpose={res.purpose}
+                                  id={res.id}
+                                  petName={res.pet_name}
+                                />
+                              );
+                            }
+                          })}
+                        </div>
+                      </th>
+                    );
+                  })}
                 </tr>
               );
             })}
@@ -141,12 +156,84 @@ const TIME = [
   { id: 5, time: "10:00" },
   { id: 6, time: "11:00" },
   { id: 7, time: "12:00" },
-  { id: 8, time: ":1300" },
+  { id: 8, time: "13:00" },
   { id: 9, time: "14:00" },
   { id: 10, time: "15:00" },
-  { id: 11, time: ":16:00" },
+  { id: 11, time: "16:00" },
   { id: 12, time: "17:00" },
   { id: 13, time: "18:00" },
+];
+
+const RESERVATION: {
+  id: number;
+  time: string;
+  date: string;
+  pet_name: string;
+  purpose: string;
+}[] = [
+  {
+    id: 1,
+    time: "10:00",
+    date: "2022-08-01",
+    pet_name: "더미",
+    purpose: "진료 예약",
+  },
+  {
+    id: 2,
+    time: "11:00",
+    date: "2022-08-03",
+    pet_name: "벨",
+    purpose: "수술 예약",
+  },
+  {
+    id: 3,
+    time: "06:00",
+    date: "2022-08-06",
+    pet_name: "두부",
+    purpose: "미용 예약",
+  },
+  {
+    id: 4,
+    time: "13:00",
+    date: "2022-08-05",
+    pet_name: "뽀삐",
+    purpose: "기타 예약",
+  },
+  {
+    id: 5,
+    time: "13:00",
+    date: "2022-08-05",
+    pet_name: "커피",
+    purpose: "수술 예약",
+  },
+  {
+    id: 6,
+    time: "13:00",
+    date: "2022-08-07",
+    pet_name: "윌리",
+    purpose: "진료 예약",
+  },
+  {
+    id: 7,
+    time: "12:00",
+    date: "2022-07-31",
+    pet_name: "희융이",
+    purpose: "미용 예약",
+  },
+  {
+    id: 8,
+    time: "13:00",
+    date: "2022-08-05",
+    pet_name: "꾸뭉이",
+    purpose: "진료 예약",
+  },
+  {
+    id: 9,
+    time: "06:00",
+    date: "2022-07-30",
+    pet_name: "해피",
+    purpose: "기타 예약",
+  },
 ];
 
 export default Calendar;
---
+++ components/ReservationItem.tsx
@@ -0,0 +1,38 @@
+import { useEffect, useState } from "react";
+
+const ReservationItem = ({
+  id,
+  petName,
+  purpose,
+}: {
+  id: number;
+  petName: string;
+  purpose: string;
+}) => {
+  const [backgroundColor, setBackgroundColor] = useState("");
+
+  useEffect(() => {
+    if (purpose === "진료 예약") {
+      setBackgroundColor("bg-[#ffe0aa]");
+    } else if (purpose === "수술 예약") {
+      setBackgroundColor("bg-[#ffd0d0]");
+    } else if (purpose === "미용 예약") {
+      setBackgroundColor("bg-[#eadefd]");
+    } else if (purpose === "기타 예약") {
+      setBackgroundColor("bg-[#e9e9f0]");
+    }
+  }, [purpose]);
+
+  return (
+    <div
+      key={id}
+      className={`${backgroundColor} h-5 rounded-[0.625rem] flex items-center justify-center m-1  `}
+    >
+      <span className="text-[0.625rem] text=[#4d4f5c] font-normal px-3">
+        {petName}
+      </span>
+    </div>
+  );
+};
+
+export default ReservationItem;
