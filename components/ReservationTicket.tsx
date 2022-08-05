6147929e64897532ee67040e6d084d18a91380a0
hasangwon <305243@naver.com>
2022-08-05 17:57:50 +0900

    Add : Aside 컴포넌트 css 수정

--- components/ReservationTicket.tsx
+++ components/ReservationTicket.tsx
@@ -23,12 +23,14 @@ export default function ReservationTicket({ doc }: { doc: DailyDataType }) {
         <div className="inline-block w-6 mt-[0.6rem] mr-[0.7rem]">{doc.animal === "dog" ? <Image src={Dog} alt="dog" /> : <Image src={Cat} alt="cat" />}</div>
         <span className="text-[1rem] font-normal">{doc.pet_name}</span>
       </div>
+
       {/* 보호자 정보 */}
       <div className="mt-2 text-[0.8125rem]">
         <span className="mr-1">{doc.owner_phone}</span>|<span className="ml-1 text-neutral-normal">{doc.owner_name} 보호자</span>
       </div>
+
       {/* 시간정보 */}
-      <div className="flex mt-6 text-[0.8125rem]">
+      <div className="flex justify-between mt-6 text-[0.8125rem]">
         <div className="flex flex-col mr-6">
           <span className=" text-neutral-light">Time</span>
           <span>{doc.visit_time}</span>
@@ -38,11 +40,12 @@ export default function ReservationTicket({ doc }: { doc: DailyDataType }) {
           <span>{doc.visit_date}</span>
         </div>
         <div className="flex-1" />
+
+        {/* 예약 변경 버튼 */}
         <div className="right-1 bottom-[1rem] py-[0.625rem] px-[0.8125rem] text-[0.75rem] text-primary border border-primary rounded-[1.25rem] select-none" onClick={() => alert(doc.id)}>
           예약 변경
         </div>
       </div>
-      {/* 예약 변경 버튼 */}
 
       {/* 예약 삭제 버튼 */}
       <div className="absolute top-6 right-7 hidden group-hover:block w-4 h-auto">
